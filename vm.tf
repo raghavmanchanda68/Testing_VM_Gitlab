a416304ef0eefdc4778aaf0be5810254cdd4e889
raghavmanchanda68 <141496172+raghavmanchanda68@users.noreply.github.com>
2024-06-12 16:04:19 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -78,5 +78,5 @@ resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefix       = "10.0.2.0/24"
+  address_prefixes       = "10.0.2.0/24"
 }

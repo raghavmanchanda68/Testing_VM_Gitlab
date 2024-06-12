a880f5a3f3ec592aa33623a8a9f4b478dee5c873
raghavmanchanda68 <141496172+raghavmanchanda68@users.noreply.github.com>
2024-06-12 12:57:42 +0530

    Create vm.tf

---
+++ vm.tf
@@ -0,0 +1,82 @@
+variable "vm_name" {
+  description = "The name of the VM"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "The name of the virtual network"
+  type        = string
+}
+
+variable "nic_name" {
+  description = "The name of the network interface"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = var.vm_name
+  location              = "West Europe"
+  resource_group_name   = var.resource_group_name
+  network_interface_id  = azurerm_network_interface.main.id
+  vm_size               = "Standard_D2s_v3"
+
+  delete_os_disk_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "main-os-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  os_profile {
+    computer_name  = var.vm_name
+    admin_username = "admin"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = var.nic_name
+  location            = "West Europe"
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = var.vnet_name
+  location            = "West Europe"
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "10.0.2.0/24"
+}

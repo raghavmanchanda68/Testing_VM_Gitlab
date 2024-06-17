7dbdf4c4d8789c51386ac96c3bfd4874fb20e0b8
raghavmanchanda68 <141496172+raghavmanchanda68@users.noreply.github.com>
2024-06-17 11:52:43 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -39,62 +39,59 @@ variable "tenant_id" {
 }
 */
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = var.vm_name
-  location              = "West US"
-  resource_group_name   = var.resource_group_name
-  vm_size               = "Standard_D2s_v3"
-
-  delete_os_disk_on_termination = true
-  network_interface_ids = [
-    azurerm_network_interface.main.id,
-  ]
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "main-os-disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
-  }
-
-  os_profile {
-    computer_name  = var.vm_name
-    admin_username = "admin"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-}
-
-resource "azurerm_network_interface" "main" {
+resource "azurerm_network_interface" "example" {
   name                = var.nic_name
-  location            = "West US"
+  location            = "westus"
   resource_group_name = var.resource_group_name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
+    subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
   }
 }
-
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "example" {
   name                = var.vnet_name
-  location            = "West US"
-  resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
+  location            = "westus"
+  resource_group_name = var.resource_group_name
 }
 
-resource "azurerm_subnet" "internal" {
+resource "azurerm_subnet" "example" {
   name                 = "internal"
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes       = ["10.0.2.0/24"]
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
+
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                            = var.vm_name
+  location                        = "westus"
+  resource_group_name             = var.resource_group_name
+  size                            = "Standard_DS1_v2"
+  computer_name                   = var.vm_name
+  admin_username                  = "adminuser"
+  admin_password                  = "admin@12345"
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  tags = {
+    Kyndryl_Managed = "true"
+    practice        = "cloud"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
+

922729f58420069e1d9f777b1066c6ca9862ebe4
raghavmanchanda68 <141496172+raghavmanchanda68@users.noreply.github.com>
2024-06-17 11:27:05 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -38,6 +38,20 @@ variable "tenant_id" {
   type        = string
 }
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.63.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-MOCPEDemo-kcl-westus-003"
+    storage_account_name = "gitlabtf"
+    container_name       = "gitlab-cont"
+    key                  = "test.terraform.tfstate"
+  }
+}
 
 provider "azurerm" {
   features {}

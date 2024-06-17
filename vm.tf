4e81730a7072b85d7d297bb93001691bccc2491d
raghavmanchanda68 <141496172+raghavmanchanda68@users.noreply.github.com>
2024-06-17 15:15:59 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -17,11 +17,11 @@ variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
 }
-variable "admin_username" {
+variable "username" {
   description = "The name of the VM"
   type        = string
 }
-variable "admin_password" {
+variable "password" {
   description = "The name of the VM"
   type        = string
 }
@@ -58,8 +58,8 @@ resource "azurerm_windows_virtual_machine" "example" {
   resource_group_name             = var.resource_group_name
   size                            = "Standard_DS1_v2"
   computer_name                   = var.vm_name
-  admin_username                  = var.admin_username
-  admin_password                  = var.admin_password
+  admin_username                  = var.username
+  admin_password                  = var.password
   disable_password_authentication = false
 
   network_interface_ids = [
